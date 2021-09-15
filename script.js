1f192ad5d712e80d4e4c8464c08909ab364c6c11
mlhadi <mlhadi1992@gmail.com>
2021-09-15 19:56:54 +0800

    Updated page, script, and css.

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    /* validate principal */
+    if (principal <= 0){
+        alert("Enter a positive number.");
+        /* bring back focus to principal input */
+        document.getElementById("principal").focus();
+        return
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    var result = document.getElementById("result");
+
+    /*update results*/
+    result.innerHTML = `If you deposit \<mark>${principal}\</mark>,
+    \<br\>at an interest rate of \<mark\>${rate}%\</mark\>
+    \<br\>You will receive an amount of \<mark\>${interest}\</mark\>,
+    \<br\>in the year \<mark\>${year}\</mark\>.\<br\>` ;
 }
-        
+
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
